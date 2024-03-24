3ff3f44e26e39a3cc5e3aa3e66fa7e49cbd681f9
AudaxLudos <james.tarrobal.777@gmail.com>
2024-03-24 20:56:45 +0800

    Add adddevitems comman

--- src/moreconsolecommands/commands/AddDevItems.java
+++ src/moreconsolecommands/commands/AddDevItems.java
@@ -2,7 +2,7 @@ package moreconsolecommands.commands;
 
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.Console;
-import org.lazywizard.console.commands.AddSupplies;
+import org.lazywizard.console.commands.*;
 
 public class AddDevItems implements BaseCommand {
     @Override
@@ -12,7 +12,29 @@ public class AddDevItems implements BaseCommand {
             return BaseCommand.CommandResult.WRONG_CONTEXT;
         }
 
-
+        new AddXP().runCommand("20000000", context);
+        new AddCredits().runCommand("100000000", context);
+        new AddStoryPoints().runCommand("1000", context);
+        new AddSkillPoints().runCommand("100", context);
+        new InfiniteSupplies().runCommand(args, context);
+        new InfiniteFuel().runCommand(args, context);
+        new AllHullmods().runCommand(args, context);
+        new AllWings().runCommand(args, context);
+        new AllWeapons().runCommand(args, context);
+        new AddShip().runCommand("onslaught", context);
+        new AddShip().runCommand("paragon", context);
+        new AddShip().runCommand("conquest", context);
+        new AddShip().runCommand("astral", context);
+        new AddShip().runCommand("dominator", context);
+        new AddShip().runCommand("champion", context);
+        new AddShip().runCommand("apogee", context);
+        new AddShip().runCommand("mora", context);
+        new AddSupplies().runCommand(args, context);
+        new AddFuel().runCommand(args, context);
+        new AddCrew().runCommand(args, context);
+        new Repair().runCommand(args, context);
+        new Storage().runCommand(args, context);
+        new DevMode().runCommand(args, context);
 
         return BaseCommand.CommandResult.SUCCESS;
     }
